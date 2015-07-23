929a5d8bb2eadcfdfd57f3955424479896fea3a0
coolgo-imac <winerdaxian@gmail.com>
2015-07-23 17:21:13 +0800

    修复了ia的bug

---
+++ ia/iafix.js
@@ -0,0 +1,16 @@
+defineClass('JPViewController', {
+            
+    detailWithParam: function(originParamDic) {
+
+            var strParam = originParamDic.objectForKey("param");
+            var dicParam =  strParam.objectFromJSONString().mutableCopy();
+            dicParam.setObject_forKey("api_version","1");
+            self.encryptWithParam(dicParam);
+            dicParam.removeObjectForKey("u_encrypt_origin");
+            
+            var paramDic = require('NSMutableDictionary').alloc().init();
+            var rParamStr = dicParam.JSONString();
+            paramDic.setObject_forKey("param",rParamStr);
+            return paramDic;
+    }
+})
