180b31c7bbf012e24f05c639662e1436cbdd853d
coolgo.me <coolgo_us@me.com>
2016-06-14 14:31:30 +0800

    v2.4.0bugfix

--- ia/iafix.js
+++ ia/iafix.js
@@ -1,8 +1,28 @@
- 
-defineClass('UIViewController', {
-	// viewDidAppear:function(animated){
-	// 	self.super().viewDidAppear(animated);
-	// 	// require('Bugtags')
-	// 	// Bugtags.setInvocationEvent(BTGInvocationEventNone)
-	// }
-})
+require('JYDeviceMainViewController, JYKitchenViewController');
+    defineClass('JYKitchenViewController', {
+        clickDummySoyabeanDevice: function() {
+            var VirtualTypeK5 = 8;
+            self.fetchDeviceCookbookWithProduceType(VirtualTypeK5);
+            var weakSelf = __weak(self);
+            dispatch_after(1.0, function(){
+            var controller = JYDeviceMainViewController.alloc().init();
+            controller.setVirtualType(VirtualTypeK5);
+            weakSelf.navigationController().pushViewController_animated(controller, true);
+            });
+        },\
+    })
+
+
+ require('JYDeviceMainViewController, JYKitchenViewController');
+      defineClass('JYKitchenViewController', {
+          clickDummyCookerDevice: function() {
+          var VirtualTypeCooker = 11266;
+          self.fetchDeviceCookbookWithProduceType(VirtualTypeCooker);
+          var weakSelf = __weak(self);
+          dispatch_after(1.0, function(){
+            var controller = JYDeviceMainViewController.alloc().init();
+            controller.setVirtualType(VirtualTypeCooker);
+            weakSelf.navigationController().pushViewController_animated(controller, true);
+          });
+        },
+    })
